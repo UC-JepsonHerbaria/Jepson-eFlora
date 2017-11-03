a6d5927bbb955d3b3b23d4905791596d22815556
jalexander <jason_alexander@berkeley.edu>
2017-11-03 16:22:57 -0700

    August 2017 version of the eflora display php page, modified by J. Alexander

--- pages/eflora_display.php
+++ pages/eflora_display.php
@@ -5,7 +5,7 @@ $TaxonID = htmlspecialchars($_GET["tid"]);
 require 'config/config.php';
 $db = new SQLite3($database_location);
 
-$results = $db->query('SELECT ID, ScientificName, TaxonAuthor, FormattedDisplayName, CommonName, NativeStatus, TJM2Author, ScientificEditor, Habit, PlantBody, Stem, SterileStem, FertileStem, Leaf, Spines, Inflorescence, StaminateHead, RayOrPistillateFlower, PistillateHead, StaminateInflorescence, PistillateOrBisexualInflorescence, PistillateInflorescence, Spikelet, FertileSpikelet, SterileSpikelet, DistalSpikelet, CentralSpikelet, LateralSpikelet, StaminateSpikelet, PistillateSpikelet, Flower, StaminateFlower, PistillateFlower, RayFlower, DiskFlower, Cone, PollenCone, SeedCone, BisexualFlower, Fruit, Seed, Sporangia, SporangiumCase, MaleSporangiumCase, FemaleSporangiumCase, Spores, Chromosomes, RarityStatus, Ecology, Elevation, BioregionalDistribution, OutsideCA, SpeciesInGenus, GeneraInFamily, Etymology, Toxicity, Synonyms, UnabridgedSynonyms, Note, UnabridgedNote, FloweringTime, FloweringTimeCode, FruitingTime, ConingTime, Weediness, IsTerminalTaxon, RevisionNumber, DistCode, HasKey 
+$results = $db->query('SELECT ID, ScientificName, TaxonAuthor, FormattedDisplayName, CommonName, NativeStatus, TJM2Author, ScientificEditor, Habit, PlantBody, Stem, SterileStem, FertileStem, Leaf, Spines, Inflorescence, StaminateHead, RayOrPistillateFlower, PistillateHead, StaminateInflorescence, PistillateOrBisexualInflorescence, PistillateInflorescence, Spikelet, FertileSpikelet, SterileSpikelet, DistalSpikelet, CentralSpikelet, LateralSpikelet, StaminateSpikelet, PistillateSpikelet, Flower, StaminateFlower, PistillateFlower, RayFlower, DiskFlower, Cone, PollenCone, SeedCone, BisexualFlower, Fruit, Seed, Sporangia, SporangiumCase, MaleSporangiumCase, FemaleSporangiumCase, Spores, Chromosomes, RarityStatus, Ecology, Elevation, BioregionalDistribution, OutsideCA, SpeciesInGenus, GeneraInFamily, Etymology, Toxicity, Synonyms, Note, UnabridgedNote, FloweringTime, FloweringTimeCode, FruitingTime, ConingTime, Weediness, IsTerminalTaxon, RevisionNumber, DistCode, HasKey 
 						FROM eflora_taxa
 						WHERE TaxonID='.$TaxonID.''); //16711 is the TaxonID for Calochortus amabilis, for example
 
@@ -58,9 +58,8 @@ $MaleSporangiumCase = $row['MaleSporangiumCase'];
 $FemaleSporangiumCase = $row['FemaleSporangiumCase'];
 $Spores = $row['Spores'];
 $Chromosomes = $row['Chromosomes'];
-//Rarity Status used to be below Ecology in sanity.pl and this file; however 90% of all records in Eflora_treatments had RarityStatus above ecology.
-//This file and others will be changed to match Eflora_treatments, which was the easiest change to make.  There are 2090+ records out of order otherwise.
-//JAA, Nov 2016
+#Rarity Status used to be below Ecology in sanity.pl and this file; however 90% of all records in Eflora_treatments had RarityStatus above ecology.
+#This file and others will be changed to match Eflora_treatments, which was the easiest change to make.  There are 2090+ records out of order otherwise.
 $RarityStatus = $row['RarityStatus'];
 $Ecology = $row['Ecology'];
 $Elevation = $row['Elevation'];
@@ -71,7 +70,6 @@ $GeneraInFamily = $row['GeneraInFamily'];
 $Etymology = $row['Etymology'];
 $Toxicity = $row['Toxicity'];
 $Synonyms = $row['Synonyms'];
-$UnabridgedSynonyms = $row['UnabridgedSynonyms'];
 $Note = $row['Note'];
 $UnabridgedNote = $row['UnabridgedNote'];
 $FloweringTime = $row['FloweringTime'];
@@ -117,7 +115,7 @@ while ($row = $results->fetchArray()) {
 
 //query and assign family info
 //Note that only Family has "GeneraInFamily" and "ScientificEditor"
-$results = $db->query('SELECT t.TaxonID, t.FamilyID, f.TaxonID, f.ScientificName, f.TaxonAuthor, f.CommonName, f.TJM2Author, f.ScientificEditor, f.Habit, f.PlantBody, f.Stem, f.SterileStem, f.FertileStem, f.Leaf, f.Spines, f.Inflorescence, f.StaminateHead, f.RayOrPistillateFlower, f.PistillateHead, f.StaminateInflorescence, f.PistillateOrBisexualInflorescence, f.PistillateInflorescence, f.Spikelet, f.FertileSpikelet, f.SterileSpikelet, f.DistalSpikelet, f.CentralSpikelet, f.LateralSpikelet, f.StaminateSpikelet, f.PistillateSpikelet, f.Flower, f.StaminateFlower, f.PistillateFlower, f.RayFlower, f.DiskFlower, f.Cone, f.PollenCone, f.SeedCone, f.BisexualFlower, f.Fruit, f.Seed, f.Sporangia, f.SporangiumCase, f.MaleSporangiumCase, f.FemaleSporangiumCase, f.Spores, f.Chromosomes, f.GeneraInFamily, f.Etymology, f.Toxicity, f.Synonyms, f.UnabridgedSynonyms, f.Note, f.UnabridgedNote, f.RevisionNumber, f.HasKey
+$results = $db->query('SELECT t.TaxonID, t.FamilyID, f.TaxonID, f.ScientificName, f.TaxonAuthor, f.CommonName, f.TJM2Author, f.ScientificEditor, f.Habit, f.PlantBody, f.Stem, f.SterileStem, f.FertileStem, f.Leaf, f.Spines, f.Inflorescence, f.StaminateHead, f.RayOrPistillateFlower, f.PistillateHead, f.StaminateInflorescence, f.PistillateOrBisexualInflorescence, f.PistillateInflorescence, f.Spikelet, f.FertileSpikelet, f.SterileSpikelet, f.DistalSpikelet, f.CentralSpikelet, f.LateralSpikelet, f.StaminateSpikelet, f.PistillateSpikelet, f.Flower, f.StaminateFlower, f.PistillateFlower, f.RayFlower, f.DiskFlower, f.Cone, f.PollenCone, f.SeedCone, f.BisexualFlower, f.Fruit, f.Seed, f.Sporangia, f.SporangiumCase, f.MaleSporangiumCase, f.FemaleSporangiumCase, f.Spores, f.Chromosomes, f.GeneraInFamily, f.Etymology, f.Toxicity, f.Synonyms, f.Note, f.UnabridgedNote, f.RevisionNumber, f.HasKey
 						from eflora_taxonomy t, eflora_taxa f
 						WHERE t.TaxonID='.$TaxonID.' AND t.FamilyID=f.TaxonID');
 
@@ -179,7 +177,7 @@ while ($row = $results->fetchArray()) {
 
 //query and assign genus info
 //Note that only Genus has "SpeciesInGenus"
-$results = $db->query('SELECT t.TaxonID, t.GenusID, g.TaxonID, g.ScientificName, g.TaxonAuthor, g.CommonName, g.TJM2Author, g.Habit, g.PlantBody, g.Stem, g.SterileStem, g.FertileStem, g.Leaf, g.Spines, g.Inflorescence, g.StaminateHead, g.RayOrPistillateFlower, g.PistillateHead, g.StaminateInflorescence, g.PistillateOrBisexualInflorescence, g.PistillateInflorescence, g.Spikelet, g.FertileSpikelet, g.SterileSpikelet, g.DistalSpikelet, g.CentralSpikelet, g.LateralSpikelet, g.StaminateSpikelet, g.PistillateSpikelet, g.Flower, g.StaminateFlower, g.PistillateFlower, g.RayFlower, g.DiskFlower, g.Cone, g.PollenCone, g.SeedCone, g.BisexualFlower, g.Fruit, g.Seed, g.Sporangia, g.SporangiumCase, g.MaleSporangiumCase, g.FemaleSporangiumCase, g.Spores, g.Chromosomes, g.SpeciesInGenus, g.Etymology, g.Toxicity, g.Synonyms, g.UnabridgedSynonyms, g.Note, g.UnabridgedNote, g.RevisionNumber, g.HasKey
+$results = $db->query('SELECT t.TaxonID, t.GenusID, g.TaxonID, g.ScientificName, g.TaxonAuthor, g.CommonName, g.TJM2Author, g.Habit, g.PlantBody, g.Stem, g.SterileStem, g.FertileStem, g.Leaf, g.Spines, g.Inflorescence, g.StaminateHead, g.RayOrPistillateFlower, g.PistillateHead, g.StaminateInflorescence, g.PistillateOrBisexualInflorescence, g.PistillateInflorescence, g.Spikelet, g.FertileSpikelet, g.SterileSpikelet, g.DistalSpikelet, g.CentralSpikelet, g.LateralSpikelet, g.StaminateSpikelet, g.PistillateSpikelet, g.Flower, g.StaminateFlower, g.PistillateFlower, g.RayFlower, g.DiskFlower, g.Cone, g.PollenCone, g.SeedCone, g.BisexualFlower, g.Fruit, g.Seed, g.Sporangia, g.SporangiumCase, g.MaleSporangiumCase, g.FemaleSporangiumCase, g.Spores, g.Chromosomes, g.SpeciesInGenus, g.Etymology, g.Toxicity, g.Synonyms, g.Note, g.UnabridgedNote, g.RevisionNumber, g.HasKey
 						from eflora_taxonomy t, eflora_taxa g
 						WHERE t.TaxonID='.$TaxonID.' AND t.GenusID=g.TaxonID');
 
@@ -239,7 +237,7 @@ while ($row = $results->fetchArray()) {
 
 //query and assign species info
 //Species has neither GeneraInFamily nor SpeciesInGenus
-$results = $db->query('SELECT t.TaxonID, t.SpeciesID, s.TaxonID, s.ScientificName, s.TaxonAuthor, s.CommonName, s.TJM2Author, s.Habit, s.PlantBody, s.Stem, s.SterileStem, s.FertileStem, s.Leaf, s.Spines, s.Inflorescence, s.StaminateHead, s.RayOrPistillateFlower, s.PistillateHead, s.StaminateInflorescence, s.PistillateOrBisexualInflorescence, s.PistillateInflorescence, s.Spikelet, s.FertileSpikelet, s.SterileSpikelet, s.DistalSpikelet, s.CentralSpikelet, s.LateralSpikelet, s.StaminateSpikelet, s.PistillateSpikelet, s.Flower, s.StaminateFlower, s.PistillateFlower, s.RayFlower, s.DiskFlower, s.Cone, s.PollenCone, s.SeedCone, s.BisexualFlower, s.Fruit, s.Seed, s.Sporangia, s.SporangiumCase, s.MaleSporangiumCase, s.FemaleSporangiumCase, s.Spores, s.Chromosomes, s.Etymology, s.Toxicity, s.Synonyms, s.UnabridgedSynonyms, s.Note, s.UnabridgedNote, s.RevisionNumber
+$results = $db->query('SELECT t.TaxonID, t.SpeciesID, s.TaxonID, s.ScientificName, s.TaxonAuthor, s.CommonName, s.TJM2Author, s.Habit, s.PlantBody, s.Stem, s.SterileStem, s.FertileStem, s.Leaf, s.Spines, s.Inflorescence, s.StaminateHead, s.RayOrPistillateFlower, s.PistillateHead, s.StaminateInflorescence, s.PistillateOrBisexualInflorescence, s.PistillateInflorescence, s.Spikelet, s.FertileSpikelet, s.SterileSpikelet, s.DistalSpikelet, s.CentralSpikelet, s.LateralSpikelet, s.StaminateSpikelet, s.PistillateSpikelet, s.Flower, s.StaminateFlower, s.PistillateFlower, s.RayFlower, s.DiskFlower, s.Cone, s.PollenCone, s.SeedCone, s.BisexualFlower, s.Fruit, s.Seed, s.Sporangia, s.SporangiumCase, s.MaleSporangiumCase, s.FemaleSporangiumCase, s.Spores, s.Chromosomes, s.Etymology, s.Toxicity, s.Synonyms, s.Note, s.UnabridgedNote, s.RevisionNumber
 						from eflora_taxonomy t, eflora_taxa s
 						WHERE t.TaxonID='.$TaxonID.' AND t.SpeciesID=s.TaxonID');
 
@@ -344,7 +342,12 @@ exit();
 ?>
 
 <head><title>Jepson Herbarium: Jepson Flora Project: Jepson eFlora: <?php echo "$ScientificName"?></title> 
-<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<!--<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">--> 
+<!--This above line has been in here for an unknown length of time, but it effectively stops all search engines from indexing this page for display in searches
+no eFlora pages will come up in searches in Google with this in here, and neither will species pages from eFlora.  CalFlora will remain the top result if this is kept.
+The alternate search, like search_IJM.pl, is the only one comming up
+in google as of August 2017.  This alternate page, however, is dated and a bit ugly, especially compared to CalFlora.  I am sprucing it up with images from CalPhotos, so it will remain.
+This is now commented out so this page will be indexed and species pages will now hopefully be included in Google searches --> 
 <link href="http://ucjeps.berkeley.edu/common/styles/dropdowns.css" rel="stylesheet" type="text/css" />
 <link href="common/css/jepson_eflora.css" rel="stylesheet" type="text/css" />
 
@@ -356,22 +359,64 @@ exit();
 <!-- google maps files and scripts -->
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript"> 
+
+	var map; //If we want access "map" outside of initialize(), this must be a global variable
+	var kml;
+	
 function initialize() {
-  var myOptions = {
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+  var mapDiv = document.getElementById('map_canvas');
+    
+  var mapOptions = {
+	mapTypeId: google.maps.MapTypeId.ROADMAP
   }
+  
+  map = new google.maps.Map(mapDiv, mapOptions);
 
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+var TaxID = "<?php echo $TaxonID?>";
 
-	var TaxID = <?php echo "$TaxonID"?>;
+var kmlLayer = new google.maps.KmlLayer("http://ucjeps.berkeley.edu/jepcodes-v7.kmz", { suppressInfoWindows: true, preserveViewport: false });
+kmlLayer.setMap(map);
 
-  var kmlLayer = new google.maps.KmlLayer("http://ucjeps.berkeley.edu/jepcodes-v7.kmz", { suppressInfoWindows: true });
-  kmlLayer.setMap(map);
+
+var pmlLayer = new google.maps.KmlLayer(
+	"http://ucjeps.berkeley.edu/eflora/KMLs/"+TaxID+".kml?122", { map: map, preserveViewport: true });
+pmlLayer.setMap(map);
+
+//         var bounds = pmlLayer.getDefaultViewport();
+//map.fitBounds(bounds);
+
+google.maps.event.addListener(pmlLayer, 'defaultviewport_changed', function() {
+   var bounds = pmlLayer.getDefaultViewport();
+   map.setCenter(bounds.getCenter());
+ // set the zoom level to 6.
+  map.setZoom(6);
+});
+
+//function initialize() {
+// This is the minimum zoom level that we'll allow
+// var maxZoomLevel = 10;
+
+//  var myOptions = {
+ //   mapTypeId: google.maps.MapTypeId.ROADMAP
+//  }
+//var map = new google.maps.Map(document.getElementById('map_canvas'), {
+//     zoom: maxZoomLevel,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+// });
+ 
+//  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+//	var TaxID = <?php echo "$TaxonID"?>;
+
+//  var kmlLayer = new google.maps.KmlLayer("http://ucjeps.berkeley.edu/jepcodes-v7.kmz", { suppressInfoWindows: true });
+//  kmlLayer.setMap(map);
 //  var pmlLayer = new google.maps.KmlLayer("http://herbaria4.herb.berkeley.edu/interchange/coords/"+TaxID+".kml", { });
-  var pmlLayer = new google.maps.KmlLayer("http://ucjeps.berkeley.edu/eflora/KMLs/"+TaxID+".kml?122", { });
-  pmlLayer.setMap(map);
+//  var pmlLayer = new google.maps.KmlLayer("http://ucjeps.berkeley.edu/eflora/KMLs/"+TaxID+".kml?122", {  });
+//  pmlLayer.setMap(map);
 
 }
+
+initialize();
 
 function JumpToIt(list) {
     var newPage = list.options[list.selectedIndex].value
@@ -456,12 +501,14 @@ $(document).ready(function(){
    <tr>
        <td>&nbsp;</td>
        <td>&nbsp;</td>
-       <td><span class="pageName"><a href="http://ucjeps.berkeley.edu/eflora/">Jepson eFlora:</a> Taxon page</span>
+       <td><span class="pageName"><a href="http://ucjeps.berkeley.edu/IJM.html">Jepson eFlora:</a> Taxon page</span>
 		<br>
 			<span class="pageAuthorLine">
 				<a href="http://ucjeps.berkeley.edu/IJM_fam_key.html">Key to families</a>
 				|
 				<a href="http://ucjeps.berkeley.edu/IJM_toc.html">Table of families and genera</a>
+				|
+				<a href="#MAP">Map</a>
 			</span>	  
 		</td>
 
@@ -768,7 +815,6 @@ if ($Note){ echo "<b>Note:</b> {$Note} ";}
 echo "<br>";
 
 if ($Synonyms){ echo "<b>Synonyms:</b> {$Synonyms}<br>";}
-if ($UnabridgedSynonyms){ echo "<b>Unabridged Synonyms:</b> {$UnabridgedSynonyms}<br>";}
 if ($UnabridgedNote){ echo "<font color='blue'><b>Unabridged Note:</b> {$UnabridgedNote}</font><br>";}
 if ($JMAuthor){ echo "<b>eFlora Treatment Author:</b> {$JMAuthor}<br>";}
 if ($ScientificEditor){ echo "<b>Scientific Editor:</b> {$ScientificEditor}<br>";}
@@ -868,11 +914,10 @@ if (isset($IllustrationFile)) {
 
 <P class=bodyText>
 <?php echo "Citation for this treatment: ".$JMAuthor." ".date ("Y").". <i>".$ScientificName."</i>, ";
-	  echo "in Jepson Flora Project (eds.) <em>Jepson eFlora</em>, ";
-	  	if ($RevNumber){ echo $RevNumber.", ";}
-	  echo "http://ucjeps.berkeley.edu/eflora/eflora_display.php?tid=".$TaxonID.", accessed on "?> <?php echo date ("F d, Y") ?>.
+if ($RevNumber){ echo $RevNumber.", ";} 
+echo "in Jepson Flora Project (eds.) <em>Jepson eFlora</em>, http://ucjeps.berkeley.edu/cgi-bin/get_IJM.pl?tid=".$TaxonID.", accessed on"?> <?php echo date ("F d, Y") //will need to update link I think ?>.
 </P>
-<P class=bodyText>Citation for the whole project: Jepson Flora Project (eds.) <?php echo date ("Y")?>. <em>Jepson eFlora</em>,  http://ucjeps.berkeley.edu/eflora/, accessed on <?php echo date ("F d, Y")?>.</P>
+<P class=bodyText>Citation for the whole project: Jepson Flora Project (eds.) <?php echo date ("Y")?>. <em>Jepson eFlora</em>,  http://ucjeps.berkeley.edu/IJM.html, accessed on <?php echo date ("F d, Y")?>.</P>
 
 
 <?php
@@ -884,17 +929,6 @@ if (is_null ($FamTaxonID)){
  }
 ?>
 
-<?php
-
-
-//for genus treatments, distribution and image pages are now excluded, they use to be displayed
-// Too many cultivated random images are showing non-california species on Genus pages
-//so exit out at this point
-if (is_null ($GenTaxonID)){
-	include('common/eflora_footer.php');
- 	exit();
- }
-?>
 
 <hr width=80%> 
 
@@ -903,20 +937,12 @@ require "$config_path"."calphotos_database_connection.php";
 
 $cp_ssp_name = str_replace(" subsp. ", " ssp. ", $ScientificName); //CalPhotos uses "ssp." and not "subsp."
 
-
 $result = mysqli_query($mysqli, "SELECT taxon, kwid, copyright FROM img 
 WHERE genre = 'Plant' 
-AND photographer IN ('Vernon Smith', 'Aaron E. Sims', 'Aaron Schusteff', 'Barry Breckling', 'Barry Rice', 'Carol W. Witham', 'Christopher Christie', 'Dana York', 'Dr. David A. Charlet', 'David Graber', 'Dean Kelch', 'Dieter Wilken', 'Gary A. Monroe', 'George W. Hartwell', 'Gerald and Buff Corsi', 'Harlan Lewis', 'Janell Hillman', 'James M. Andre', 'James Morefield', 'John Game', 'Julie Kierstead Nelson', 'Keir Morse', 'Larry Blakely', 'Lech Naumovich', 'Len Lindstrand III', 'Michael Charters', 'Michael G. Simpson', 'Neal Kramer', 'Bob Patterson', 'Robert E. Preston, Ph.D.', 'Roxanne Bittman', 'Ryan O\'Dell', 'Steve Matson', 'Steve Schoenig', 'Thomas M. Elder, M.D.', 'Thomas Stoughton', 'Toni Corelli', 'Tony Morosco', 'Dylan Neubauer')
+AND photographer IN ('Vernon Smith','Aaron E. Sims', 'Aaron Schusteff', 'Barry Breckling', 'Barry Rice', 'Carol W. Witham', 'Christopher Christie', 'Dana York', 'Dr. David A. Charlet', 'David Graber', 'Dean Kelch', 'Dieter Wilken', 'Gary A. Monroe', 'George W. Hartwell', 'Gerald and Buff Corsi', 'Harlan Lewis', 'Janell Hillman', 'James M. Andre', 'James Morefield', 'John Game', 'Julie Kierstead Nelson', 'Keir Morse', 'Larry Blakely', 'Lech Naumovich', 'Len Lindstrand III', 'Michael Charters', 'Michael G. Simpson', 'Neal Kramer', 'Bob Patterson', 'Robert E. Preston, Ph.D.', 'Roxanne Bittman', 'Ryan O\'Dell', 'Steve Matson', 'Steve Schoenig', 'Thomas M. Elder, M.D.', 'Thomas Stoughton', 'Toni Corelli', 'Tony Morosco', 'Dylan Neubauer')
 AND taxon LIKE '$cp_ssp_name%' 
-AND taxon NOT LIKE 'Echinopsis%'
 ORDER BY RAND() LIMIT 6;");
 
-//CalPhotos has specimens of the south American Cactus genus Echinopsis which the code above cannot distinguish from Echinops (asteraceae) at the genus level.
-//pcitures of the cactus are displaying on the genus page for Echinops
-//NOT LIKE line added to eliminate specimens of this genus from genus pages in the eFlora
-//AND state = '267588'
-//AND state LIKE 'CA'
-//AND captivity = '0'
  if (!$result) {
     die("<p>Error in executing the CalPhotos query : " .
         mysql_error() . "</p>");
@@ -947,6 +973,16 @@ if (isset($kwid)){ //if there is no $kwid (i.e. no photos), do not print the "se
 }
 ?>
 
+<?php
+//for genus treatments, distribution pages are excluded
+//so exit out at this point
+if (is_null ($GenTaxonID)){
+	include('common/eflora_footer.php');
+ 	exit();
+ }
+?>
+
+<a name="MAP" id="MAP"></a>
 <!--distribution panes-->
 <table border=0>
 <tr><td valign='bottom'>Geographic subdivisions for <?php echo "$ScientificName"?>:
