cf190dafc42e1915386961ba7d6a696cbc56445c
jalexander <jason_alexander@berkeley.edu>
2017-11-13 10:43:53 -0800

    2016 version of the Heath Bar script, modified by David Baxter

--- YELLOW_FLAGGING/make_heath_bar.pl
+++ YELLOW_FLAGGING/make_heath_bar.pl
@@ -1,19 +1,17 @@
-open(IN, "/Library/Webserver/CGI-Executables/_HB2.pl") || die;
-open(OUT, ">HB2.pl") || die;
+open(IN, "_hb2.pl") || die;
+open(OUT, ">hb2.pl") || die;
 while(<IN>){
 print OUT;
 last if m/^__END__/;
 }
 close(IN);
 use BerkeleyDB;
-$data_path	="/Users/richardmoe/4_CDL_BUFFER/buffer/";
+$data_path	="/usr/local/web/ucjeps_data/ucjeps_data";
 #data files
-#$CDL_name_list_file	="${data_path}CDL_name_list.txt";
-$CDL_DBM_file	="${data_path}CDL_DBM";
-$CDL_nomsyn_file	="/Users/richardmoe/4_CDL_BUFFER/buffer/stats/CDL_nomsyn";
-$CDL_taxsyn_file	="/Users/richardmoe/4_CDL_BUFFER/buffer/stats/CDL_taxsyn";
-tie(%TNOAN, "BerkeleyDB::Hash", -Filename=>"/Users/richardmoe/4_CDL_BUFFER/buffer/CDL_TID_TO_NAME", -Flags=>DB_RDONLY)|| die "$!";
-
+$CDL_name_list_file	="${data_path}/CDL_name_list.txt";
+$CDL_DBM_file	="${data_path}/CDL_DBM";
+$CDL_nomsyn_file	="${data_path}/CDL_nomsyn";
+$CDL_taxsyn_file	="${data_path}/CDL_taxsyn";  #this has not been modified since 2011, check to see how created
 tie(%nomsyns, "BerkeleyDB::Hash", -Filename=>"$CDL_nomsyn_file", -Flags=>DB_RDONLY)|| die "$!";
 #while(($key,$value)=each(%nomsyns)){
 #print "$key $value\n" if $key=~/carnosa/;
@@ -31,108 +29,73 @@ $taxsyn{$syn}=$key;
 
 tie %CDL, "BerkeleyDB::Hash", -Filename=>"$CDL_DBM_file", -Flags=>DB_RDONLY or die "Cannot open file CDL_DBM: $! $BerkeleyDB::Error\n" ;
 
-#open(IN, "/Users/richardmoe/4_data/taxon_ids/smasch_taxon_ids.txt") || die "Couldn't open the TID file\n";;
-#open(IN,"/Users/rlmoe/CDL_buffer/buffer/tnoan.out") || die;
-#while(<IN>){
-#chomp;
-#($code,$name,@residue)=split(/\t/);
-#$TNOAN{$code}=$name;
-#}
+open(IN, "${data_path}/smasch_taxon_ids.txt") || die "Couldn't open the TID file\n";;
+while(<IN>){
+chomp;
+($code,$name,@residue)=split(/\t/);
+$TNOAN{$code}=$name;
+}
 #get list of relevent names
 #<a href="/cgi-bin/get_IJM.pl?tid=29483">        Jacaranda mimosifolia</a><br>
 #<a href="/cgi-bin/get_IJM.pl?tid=4770"> Jacobaea vulgaris*</a><br>
-foreach $letter (A .. Z){
-	#open(IN, "/Users/richardmoe/4_IJM/2013/2013_JUNE/IJM_index_${letter}.html") || die;
-	open(IN, "${data_path}/stats/IJM_index_${letter}.html") || die;
-	while(<IN>){
-		next unless /tid=\d/;
-		next if m/\*<\/a>/;
-		chomp;
-		s/^.*">[ 	]*//;
-		s/<\/a>.*//;
-		s/(subsp\.|var\.|f\.) //;
-s/&times;/X /;
-$name=lc($_);
-#print "$name\n";
-		$wanted_name{$name}=$name;
-		if($nomsyns{$name}){
-			#print "$name: $nomsyns{$name}\n";
-			@syns=split(/\t/,$nomsyns{$name});
-			foreach $syn (@syns){
-				$wanted_name{$syn}=$name;
-print nowhere <<EOP;
-				>$syn< -> >$wanted_name{$syn}<
-EOP
-			}
+
+#list of accession IDs associated with mexican records
+open(IN,"data_inputs/all_mex.txt") || die;
+while(<IN>){
+chomp;
+$exclude_mexican_records{$_}++;
+}
+close(IN);
+
+while(($key,$value)=each(%CDL)){
+	@fields=split(/\t/,$value);
+	
+
+	($taxon=$fields[0])=~s/.* //;
+	($taxon=$TNOAN{$taxon})=~s/(subsp\.|var\.|f\.) //;
+	#three criteria to exclude Baja California records based on accession_id, county and/or latitude
+	next if $exclude_mexican_records{$key};
+	if ($fields[8] =~ /(Ensenada|Mexicali|Tecate|Tijuana|Rosarito)/){
+		#warn "$taxon skipped, Mexican county\n";
+		next;
+	}
+	if ($fields[11] < 32.53){
+		#warn "$taxon skipped, latitude south of California\n";
+		next;
+	}
+	$taxon=lc($taxon);
+	if($taxsyn{$taxon}){
+		$taxon=$taxsyn{$taxon};
+	}
+	$collector=$fields[1];
+	$collector=~s/,? (and|&).*//;
+	$collector=~s/,.*//;
+	$collector=~s/^.*[A-Z][a-z].*//;
+	next if $seen{"$collector$taxon$fields[5]$fields[3]"}++;
+	next unless $fields[5];
+	if($fields[3]){
+		#next if $seen{$fields[3].$fields[5]}++;
+		if ($year > $this_year){
+			warn "$year $_\n";
+			next;
+		}
+		if($fields[5]==$fields[6]){
+			my ($year,$month,$day)=inverse_julian_day($fields[5]);
+        		$month=("",Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec)[$month];
+			warn "$fields[5]>>>$_\n" unless $month;
+			$store{$taxon}{$month}++;
+			$total{$taxon}++;
 		}
 	}
 }
 
-foreach $name(%wanted_name){
-print nowhere ">$name< -> >$wanted_name{$name}<\n";
-}
-#die;
+#fields[1] = collectors
+#fields[2] = CNUM_prefix
+#fields[3] = CNUM
+#fields[4] = CNUM_suffix
+#fields[5] = EJD
+#fields[6] = LJD
 
-
-
-#open(IN, "/Users/richardmoe/4_CDL_BUFFER/buffer/CDL_main.in") || die;
-#while(<IN>){
-while(($key,$value)=each(%CDL)){
-	@fields=split(/\t/,$value);
-	($taxon=$fields[0])=~s/.* //;
-	($taxon=$TNOAN{$taxon})=~s/(subsp\.|var\.|f\.) //;
-$taxon=lc($taxon);
-#print <<EOP;
-#$taxon
-#EOP
-if($taxsyn{$taxon}){
-#print <<EOP;
-#TAX $taxon=$taxsyn{$taxon};
-#EOP
-$taxon=$taxsyn{$taxon};
-}
-if($wanted_name{$taxon}){
-$taxon=$wanted_name{$taxon};
-#print <<EOP;
-#WANT $taxon=$wanted_name{$taxon};
-#EOP
-}
-else{
-#print "$taxon not wanted\n";
-next;
-}
-$collector=$fields[1];
-$collector=~s/,? (and|&).*//;
-$collector=~s/,.*//;
-$collector=~s/^.*[A-Z][a-z].*//;
-#SKIP DUPS
-next if $seen{"$collector$taxon$fields[5]$fields[3]"}++;
-#REQUIRE DATE
-	unless ($fields[5]){
-#print "$key no fields[5]\n";
-next;
-}
-#REQUIRE COLL NUM
-	if($fields[3]){
-		#next if $seen{$fields[3].$fields[5]}++;
-			if ($year > $this_year){
-				warn "$year $_\n";
-				next;
-			}
-#REQUIRE SINGLE DAY DATE
-			if($fields[5]==$fields[6]){
-				my ($year,$month,$day)=inverse_julian_day($fields[5]);
-        			$month=("",Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec)[$month];
-				warn "$fields[5]>>>$_\n" unless $month;
-				$store{$taxon}{$month}++;
-				$total{$taxon}++;
-			}
-
-	}
-else{
-#print "$key no fields[3]\n";
-}
-}
 #die;
 foreach $taxon (sort(keys(%store))){
 print OUT "$taxon\t";
