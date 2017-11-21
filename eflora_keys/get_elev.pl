1102ebf96d4f0b8696452b8f65075acb97d98b9b
jalexander <jason_alexander@berkeley.edu>
2017-11-20 16:20:40 -0800

    2013 version of elevation stats script, modified by David Baxter

--- eflora_keys/get_elev.pl
+++ eflora_keys/get_elev.pl
@@ -1,50 +1,51 @@
-open(IN,"tnoan.out") || die;
+use BerkeleyDB;
+ my $filename = "JEPS_elev.hash";
+    tie %jeps_elev, "BerkeleyDB::Hash",
+                -Filename => $filename,
+        or die "Cannot open file $filename: $! $BerkeleyDB::Error\n" ;
+
+open(IN,"/Users/richardmoe/4_data/taxon_ids/smasch_taxon_ids.txt") || die;
 while(<IN>){
 chomp;
 ($code,$name)=split(/\t/);
 $TNOAN{$name}=$code;
 }
-foreach $file (@ARGV){
-	undef($/);
-	open(IN,$file) || die "couldn't open $file\n";
-	$all_lines=<IN>;
-#converts windows line ends#
-	$all_lines=~s/\xEF\xBB\xBF//;
-	$all_lines=~s/ *\r\n/\n/g;
-	$all_lines=~s/ +/ /g;
-	@all_pars=split(/\n\n+/,$all_lines);
-	foreach(@all_pars){
-($first_line)=m/^(.*)/;
-if(m/([A-Z]+ACEAE)/){
-$family=$1;
-#print "$family\n";
-}
-if( m/NATIVE/){
-$nativity="N";
-}
-else{
-$nativity="A";
-}
-    if(m/([A-Z-]+ .*)\nTAXON AUTHOR/){
-        $name=&strip_name($1);
-    }
-    elsif(m/(NATIVE|NATURALIZED)\n([A-Z-]+ .*)/){
-        $name=&strip_name($2);
-}
-else{
-warn "skipping $first_line following $name\n";
-next;
-}
+open(WARNING,">stderr.txt") || die;
+$/="";
+$file="eflora_treatments.txt";
+open(IN,$file) || die "couldn't open $file\n";
+while(<IN>){
+	($first_line)=m/^(.*)/;
+	if(m/([A-Z]+ACEAE)/){
+		$family=$1;
+		#print "$family\n";
+	}
+	if( m/NATIVE/){
+		$nativity="N";
+	}
+	else{
+		$nativity="A";
+	}
+    	if(m/([A-Z-]+ .*)\nTAXON AUTHOR/){
+        	$name=&strip_name($1);
+    	}
+    	elsif(m/(NATIVE|NATURALIZED)\n([A-Z-]+ .*)/){
+        	$name=&strip_name($2);
+	}
+	else{
+		print WARNING "skipping $first_line following $name\n";
+		next;
+	}
 #print "$name\n" if $nativity eq "N";
 #next;
-			if(($elevation)=m/ELEVATION: (.*)/){
-if(m/UNABR.*elevation/){
-$comment=2;
-}
-else{
-$comment=1;
-}
-$max_e=$min_e="";
+	if(($elevation)=m/ELEVATION: (.*)/){
+		if(m/UNABR.*elevation/){
+			$comment=2;
+		}
+		else{
+			$comment=1;
+		}
+		$max_e=$min_e="";
 				if($elevation=~m/([0-9-]+)--([0-9-]+)/){
 					$max_e=$2; $min_e=$1;
 				}
@@ -60,29 +61,33 @@ $max_e=$min_e="";
 				elsif($elevation=~m/(^[0-9]+) m/){
 					$max_e=$1; $min_e=$1;
 				}
-else{
-print "$name $elevation\n";
-}
-					$max_e=0 if $max_e <0;
-					$min_e=0 if $min_e <0;
-					$max_e= $min_e=0 if $elevation=~/below.*tide/;
+				else{
+					print "$name $elevation\n";
+				}
+				$max_e=0 if $max_e <0;
+				$min_e=0 if $min_e <0;
+				$max_e= $min_e=0 if $elevation=~/below.*tide/;
 				$native{$name}{max}=$max_e;
 				$native{$name}{min}=$min_e;
-				print "$name\tACC\t$min_e\t$max_e\t$comment\t$nativity\n";
+				print "$name\t$min_e\t$max_e\n";
+$jeps_elev{$name}="$min_e\t$max_e";
 				if(m/SYNONYMS: (.*)/){
 					while(s/SYNONYMS: +(.*)//){
                                 		@syns=split(/; +/,$1);
                                 		foreach(@syns){
                                         		s/Expanded author citation: //;
                                         		s/_//g;
-next if s/, .*//;
-                                        		print &strip_name($_), "\tSYN\t$min_e\t$max_e\t$comment\t$nativity\n";
+							next if s/, .*//;
+                                        		$name= &strip_name($_);
+$jeps_elev{$name}="$min_e\t$max_e";
+                                        		print "$name\t$min_e\t$max_e\n";
                                 		}
                         		}
 				}
 			}
 		}
-	}
+untie(%jeps_elev);
+
 foreach $name (sort(keys(%native))){
 #print "$TNOAN{$name} $native{$name}{max}\n" if ($native{$name}{max} && $native{$name}{max}< 300);
 }
