604687dcd0844d7d7f75deda883ba5160b886520
jalexander <jason_alexander@berkeley.edu>
2017-11-22 17:35:29 -0800

    2017 version of the genus list script, modified by J. Alexander

--- eflora_keys/make_genus_lists.pl
+++ eflora_keys/make_genus_lists.pl
@@ -1,16 +1,15 @@
 use BerkeleyDB;
 
-open(IN, "/Users/richardmoe/4_data/taxon_ids/smasch_taxon_ids.txt") || die;
+open(IN, "/Users/davidbaxter/DATA/smasch_taxon_ids.txt") || die;
 local($/)="\n";
 while(<IN>){
 	chomp;
-	#s/X /&times;/;
+	s/X /\327/;
 	($code,$name,@residue)=split(/\t/);
 	$TNOAN{$name}=$code;
 	$NAN{$code}=$name;
 }
-$TNOAN{"Centaurea jacea nothosubsp. pratensis"}=93858;
-$NAN{93858}="Centaurea jacea nothosubsp. pratensis";
+
 close(IN);
 
 open(IN, "index_full.tmp") || die;
@@ -18,8 +17,7 @@ while(<IN>){
 	next if m/\*/;
 	chomp;
 	($genus=$_)=~s/ .*//;
-#s/&times;/\327 /;
-s/&times;/X /;
+s/&times;/\327/;
 	if($TNOAN{$_}){
 		$g_list{$TNOAN{$genus}}.="$TNOAN{$_},";
 	}
@@ -30,16 +28,9 @@ warn "$_\n";
 tie(%IJM, "BerkeleyDB::Hash", -Filename=>"IJM.hash", -Flags      => DB_CREATE)|| die "Stopped; couldnt open IJM\n";
 foreach(keys(%g_list)){
 $g_list{$_}="<sp_list>".join(",",sort {$NAN{$a} cmp $NAN{$b}}(split(/,/,$g_list{$_})))."</sp_list>";
-print "$NAN{$_}: $g_list{$_}\n";
+#print "$NAN{$_}: $g_list{$_}\n";
 if($IJM{$_}){
-
-
-
 $IJM{$_}.= $g_list{$_};
-#$IJM{$_}=~ s!<sp_list>.*</sp_list>!$g_list{$_}!;
-
-
-
 #print "YES $NAN{$_}: $g_list{$_}\n";
 #$IJM{$_}=~ s/<\/splist>/<\/sp_list>/;
 }
