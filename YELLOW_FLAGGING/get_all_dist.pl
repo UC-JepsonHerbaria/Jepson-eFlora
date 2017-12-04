d4d40a6fecfb5c07f91a9c7fff5ee9c61f218be3
jalexander <jason_alexander@berkeley.edu>
2017-12-04 13:57:14 -0800

    Nov 2016 version of the HCODE conversion script, modified by J. Alexander

--- YELLOW_FLAGGING/get_all_dist.pl
+++ YELLOW_FLAGGING/get_all_dist.pl
@@ -1,6 +1,6 @@
 #This reads the eFlora treatment file, storing bioregions against taxon IDs
 #Species containing infras are given the sum of included infras
-# Synonyms are assigned the range od the accepted name
+# Synonyms are assigned the range of the accepted name
 
 use BerkeleyDB;
 
@@ -15,25 +15,37 @@ while(<IN>){
 close(IN);
 
 #tie the CCH nomenclatural synonym file
-tie(%NS, "BerkeleyDB::Hash", -Filename=>"/usr/local/web/ucjeps_data/ucjeps_data/CDL_nomsyn")|| die "Stopped; couldnt open name_to_code\n";
+tie(%NS, "BerkeleyDB::Hash", -Filename=>"/usr/local/web/ucjeps_data/ucjeps_data/CDL_nomsyn")|| die "Stopped; couldnt open nomsyn file\n";
+
+foreach (keys(%NS)){
+next unless m/juncus /;
+print "$_: $NS{$_}\n";
+}
 
 #Read in the taxon ids
-#open(IN,"data_inputs/smasch_taxon_ids.txt") || die;
-#while(<IN>){
-	#chomp;
-	#s/× /&times;/;
-	#($code,$name)=split(/\t/);
-	#$TNOAN{$name}=$code;
-	#$CODE_TO_NAME{$code}=$name;
-#}
-#close(IN);
+open(IN,"/usr/local/web/ucjeps_data/ucjeps_data/smasch_taxon_ids.txt") || die;
+while(<IN>){
+	chomp;
+#	#s/× /&times;/;
+	($code,$name)=split(/\t/);
+	$NAME_TO_CODE{$name}=$code;
+	$CODE_TO_NAME{$code}=$name;
 
-tie(%TNOAN, "BerkeleyDB::Hash", -Filename=>"/usr/local/web/ucjeps_data/ucjeps_data/name_to_code", -Flags=>DB_RDONLY)|| die "$!";
-#print "26556 $TNOAN{26556}\n";
-#foreach (keys(%TNOAN)){
-#next unless m/canariensis/;
-#print "$_: $TNOAN{$_}\n";
-#}
+foreach ($name){
+next unless m/canariensis/;
+print "$CODE_TO_NAME{$code}: $NAME_TO_CODE{$name}\n";
+}
+
+}
+close(IN);
+
+tie(%TID, "BerkeleyDB::Hash", -Filename=>"/usr/local/web/ucjeps_data/ucjeps_data/CDL_TID_TO_NAME", -Flags=>DB_RDONLY)|| die "Stopped; couldnt open TID to Name file\n";
+$test = "Zostera pacifica";
+print "48951==>Zostera pacifica: $TID{$test}\n";
+
+tie(%TNOAN, "BerkeleyDB::Hash", -Filename=>"/usr/local/web/ucjeps_data/ucjeps_data/name_to_code.hash", -Flags=>DB_RDONLY)|| die "Stopped; couldnt open Name to Code file\n";
+$id_test = "48951";
+print "Zostera pacifica==>48951: $TNOAN{$id_test}\n";
 #die;
 
 
@@ -93,17 +105,20 @@ s/PROOF:.*\n?//g;
 		s|^([A-Z]+ACEAE) $|<center><font size="4"><b>$1</b></font>|ms;
 		s|^([A-Z]+ACEAE) \((.*)\)$|<center><font size="4"><b>$1 ($2)</b></font>|ms;
 		if(m/\n([A-Z][A-Z-]+ [a-z&][;a-z-]+.*)/){
-			$name_for_hc=&strip_name($1);
+			$name_1 = ucfirst(lc($1));
+			$name_for_hc=&strip_name($name_1);
 		}
 
 		if (m/DISTRIBUTION/){
 			($cal_dist)=m/BIOREGIONAL DISTRIBUTION: (.*)/;
 			$cal_dist=~s/[.; ]*$//;
 			$cal_dist=~s/\//, /g;
+			$cal_dist=~s/possibly //;
+
 			$hc=&get_hcode($cal_dist);
 #if( $name_for_hc=~/Eriogonum.*polyanthum/i){
 if ($TNOAN{$name_for_hc}){
-#print "$TNOAN{$name_for_hc} eflora_treatments.txt\t$hc\n";
+print "$TNOAN{$name_for_hc} eflora_treatments.txt\t$hc\n";
 }
 else{
 warn "$name_for_hc no code\n";
@@ -181,6 +196,16 @@ warn "$name_for_hc no code\n";
             			@syns=split(/; +/,$1);
             			foreach(@syns){
 #Skip synonyms with an indication of misapplication or other synonym qualifier
+					s/\[//g;
+					s/Expanded author citation: //;
+					s/ f\. ex /filius ex /; #fix a special case where the parser thinks this is a forma
+					s/ F\. Muell\./ Muell./i; #fix a special case where the parser thinks this is a forma
+					s/Sess&eacute; ex Lag.//i; #fix a special case where the parser thinks this another taxon
+					s/not juncus nodatus coville/juncus nodatus coville, misappl/i;
+					s/& moc\. ex dc\. subsp\./ subsp./i;
+					s/_//;
+
+
 next if m/, misappl/;
 next if m/, as treated by Munz/;
 next if m/, illeg/;
@@ -197,9 +222,8 @@ next if m/, orth\. var/;
 next if m/, poss\. illeg/;
 next if m/, possibly misappl/;
 #s/\327/X/g;
-					s/probably //;
-					s/Expanded author citation: //;
-                			s/_//g;
+					
+                			
                 			$TJM2{&strip_name($_)}.="$LAN\n";
                 			#print &strip_name($_),"\t$cal_dist\t$hc\n";
 					if($TNOAN{&strip_name($_)}){
@@ -322,7 +346,7 @@ print "4. $_ $name_for_hc : $hstr\n";
 
 
 ###############################assign string of regions to TID
-open(OUT, ">outputs/tid_dist_string.out") || die;
+open(OUT, ">outputs/tid_DIST_STRING_out.txt") || die;
 
 {
 local($/="\n");
@@ -344,7 +368,7 @@ while(<IN>){
 }
 close(IN);
 }
-#print join(" ",@regions),"\n";
+print join(" ",@regions),"\n";
 
 #this sort is important, since it establishes the order of the bit vector,
 #which should have been generated following a similar sort
@@ -384,20 +408,127 @@ print OUT "$tid\t";
 print OUT "$regions[$r],";
 				}
 		}
-#$DIST_STRING{$_}=$hstr;
+$DIST_STRING{$_}=$hstr;
 print OUT  "\n";
 }
 close(OUT);
 
 sub strip_name{
 local($_) = @_;
-s/&times;([a-z-]+).*/X $1/||
-       s/^([A-Z][-A-Za-z]+) (X?[-a-z]+).*\b(nothosubsp\.|subsp\.|ssp\.|var\.|f\.) ([-a-z]+).*/$1 $2 $3 $4/ ||
-s/^([A-Z][A-Za-z]+) ([a-z&][a-z;-]+).*/$1 $2/;
+
+
+s/ spp\./ subsp./;
 s/ssp\./subsp./;
+s/ ssp / subsp. /;
+s/ subsp / subsp. /;
+s/ var / var. /;
+s/ var. $//;
+s/ sp\..*//;
+s/ sp .*//;
+s/ ?[Uu]ndet\.?.*//;
+s/ ?[Ii]ndet\.?.*//;
+s/  +/ /g;
+s/^ +//;
+s/ +$//;
+
+s/ aff\. / /;
+s/ cf\. / /;
+s/ c\. *f\.? / /;
+
+#add additional 'filial' authorities at this point so they parse correctly, JAA
+s/Ait\. f\./Ait. filius/g;
+s/Backh\. f\./Backh. filius/g;
+s/Baker f\./Baker filius/g;
+s/Bakh\. f\./Bakh. filius/g;
+s/Balf\. f\./Balf. filius/g;
+s/Burm\. f\./Burm. filius/g;
+s/Brunner,? f\./Brunner filius/g;
+s/Celakovsky f\./L.F. Celak./g;
+s/Delar\. f\./Delar. filius/g;
+s/Forst\. f\./Forst. filius/g;
+s/Gaertn\. f\./Gaertn. filius/g;
+s/Gagnaire f\./Gagnaire filius/g;
+s/Haage f\./Haage filius/g;
+s/Haller f\./Haller filius/g;
+s/Hallier f\./Hallier filius/g;
+s/Harr\. f\./Harr. filius/g;
+s/Hedw\. f\./Hedw. filius/g;
+s/Hirats\. f\./Hirats. filius/g;
+s/Hook\. f\./Hook. filius/g;
+s/Jacq\. f\./Jacq. filius/g;
+s/Kickx f\./Kickx filius/g;
+s/Kampm\. f\./Kampm. filius/g;
+s/Keng f\./Keng filius/g;
+s/Klokov f\./Klokov filius/g;
+#s/L\. f\. sulcat/f. sulcat/g;
+
+s/L\. f\. var./L. filius var./g;
+s/L\. f\. subsp./L. filius subsp./g;
+s/L\. f\./L. filius/g;
+s/L\. *filius azure/L. f. azure/g; #fix special cases where L. f. is a forma name not filius, Anagallis arvensis L. f. azurea Hyl.
+s/L\. *filius vine/L. f. vine/g; #fix special cases where L. f. is a forma name not filius, Allium vineale L. f. vineale
+s/L\. *filius comp/L. f. comp/g; #fix special cases where L. f. is a forma name not filius, Allium vineale L. f. compactum L.
+s/L\. *filius tome/L. f. tome/g; #fix special cases where L. f. is a forma name not filius, Acer rubrum L. f. tomemtosum L. 
+s/L\. *filius sang/L. f. sang/g; #fix special cases where L. f. is a forma name not filius, Acer negundo L. f. sanguineum L. Martin
+s/L\. *filius masc/L. f. masc/g; #fix special cases where L. f. is a forma name not filius, Orchis morio L. f. mascula L.
+s/L\. *filius lute/L. f. lute/g; #fix special cases where L. f. is a forma name not filius, Ophrys insectifera L. f. luteomarginata L. Lewis
+s/L\. *filius micr/L. f. micr/g; #fix special cases where L. f. is a forma name not filius, Jasminum humile L. f. microphyllum L.C. Chia
+s/L\. *filius sask/L. f. sask/g; #fix special cases where L. f. is a forma name not filius, Carex capillaris L. f. saskatschewana (Boeckeler) L.H. Bailey
+s/L\. *filius negl/L. f. negl/g; #fix special cases where L. f. is a forma name not filius, Celtis occidentalis L. f. neglecta L.
+s/L\. *filius duri/L. f. duri/g; #fix special cases where L. f. is a forma name not filius, Eryngium campestre L. f. duriberum L.
+s/L\. *filius glab/L. f. glab/g; #fix special cases where L. f. is a forma name not filius, Dioscorea villosa L. f. glabrata L.
+s/L\. *filius prae/L. f. prae/g; #fix special cases where L. f. is a forma name not filius, Silphium trifoliatum L. f. praecisum L.M. Perry
+s/L\. *filius pinn/L. f. pinn/g; #fix special cases where L. f. is a forma name not filius, Quercus alba L. f. pinntifida L.
+s/L\. *filius pinn/L. f. vill/g; #fix special cases where L. f. is a forma name not filius, Cardiospermum corindum L. f. villosum (Mill.) Radlk
+s/Lestib\. f\./Lestib. filius/g;
+s/Lindb\. f\./Lindb. filius/g;
+s/Lindm\. f\./Lindm. filius/g;
+s/Luer f\./Luer filius/g;
+s/Michx\. f\./F. Michx./g;
+s/Michaux f\./F. Michx./g;
+s/Occhioni f\./Occhioni filius/g;
+s/Pearsall f\./Pearsall filius/g;
+s/Phil\. f\./Phil. filius/g;
+s/Rchb\. f\./Rchb. filius/g;
+s/Rech\. f\./Rech. filius/g;
+s/Rehb\. f\./Rehb. filius/g;
+s/Reichenb\. f\./Reichenb. filius/g;
+s/Reichenbach f\./Reichenbach filius/g;
+s/Scheuchzer f\./Scheuchzer filius/g;
+s/Schultes f\./Schultes filius/g;
+s/Schult\. f\./Schult. filius/g;
+s/Wallr\. f\./Wallr. filius/g;
+s/Wendl\. f\./Wendl. filius/g;
+
+#s/^([A-Z][A-Za-z]+) (X?[-a-z]+).*? (subvar\.|subsp\.|ssp\.|var\.|subvar\.|f\.|nothosubsp\.) ([-a-z]+).*/$1 $2 $3 $4/ ||
+#s/^([A-Z][A-Za-z]+) X ([-a-z]+) .+/$1 X $2/||
+#s/^([A-Z][A-Za-z]+) × ?([-a-z]+) .+/$1 X $2/||
+#s/^([A-Z][A-Za-z]+) × ?([-a-z]+)/$1 X $2/||
+#s/^([A-Z][A-Za-z]+) (X?[-a-z]+) .+/$1 $2/||
+#s/^([A-Z][A-Za-z]+) (indet\.|sp\.)/$1 indet./||
+#s/^([A-Z][A-Za-z]+) (X?[-a-z]+)/$1 $2/||
+#s/^([A-Z][A-Za-z]+) (X [-a-z]+)/$1 $2/||
+#s/^X (A-Z][a-z]+) ([-a-z]+) (.+)/X $2 $3/||
+#s/^([A-Z][A-Za-z]+) (.+)/$1/;
+#s/ssp\./subsp./;
+#s/ +$//;
 $_= ucfirst(lc($_));
-$_=~s/ x / X /;
-s/\327/X/;
+
+s/&times;([a-z-]+).*/X $1/||
+s/^([A-Z][A-Za-z]+) ([-a-z]+) ?.* (subvar\.|subsp\.|var\.|f\.|nothosubsp\.) ([-a-z]+).*/$1 $2 $3 $4/ ||
+s/^([A-Z][A-Za-z]+) X ([-a-z]+) .+/$1 X $2/||
+s/^([A-Z][A-Za-z]+) ([-a-z]+) .+/$1 $2/||
+s/^([A-Z][A-Za-z]+) ([-a-z]+)/$1 $2/||
+s/^([A-Z][A-Za-z]+) (X [-a-z]+)/$1 $2/||
+s/^X (A-Z][a-z]+) ([-a-z]+) (.+)/X $2 $3/||
+s/^([A-Z][A-Za-z]+) (.+)/$1/;
+
+s/  +/ /;
+s/ +$//;
+s/^ +//;
+
+
+
 $_;
 }
 
